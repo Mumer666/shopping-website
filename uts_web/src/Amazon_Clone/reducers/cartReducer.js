a4481e6960ef3c112ff494562997687cf4ceaf49
Mumer666 <81896836+Mumer666@users.noreply.github.com>
2024-03-29 10:19:21 +0500

    fixed cart total bill calculation

--- uts_web/src/Amazon_Clone/reducers/cartReducer.js
+++ uts_web/src/Amazon_Clone/reducers/cartReducer.js
@@ -1,26 +1,26 @@
-const CartReducer = (state , action) => {
+const CartReducer = (state, action) => {
 
     switch (action.type) {
         case "add_cart_data":
-            const {colour , quantity , singleProduct} = action.payload;
+            const { colour, quantity, singleProduct } = action.payload;
             let cartDetails = {
                 id: singleProduct.id + colour,
-                name : singleProduct.name,
-                quantity : quantity,
-                price : singleProduct.price,
-                colour : colour,
-                image : singleProduct.image[0].url,
+                name: singleProduct.name,
+                quantity: quantity,
+                price: singleProduct.price,
+                colour: colour,
+                image: singleProduct.image[0].url,
 
             }
-            return{
+            return {
                 ...state,
-                cart:[...state.cart , cartDetails],
+                cart: [...state.cart, cartDetails],
             }
         case "clear_cart_item":
             let id = action.payload;
             let remainingCart = state.cart.filter((curElem) => {
                 return curElem.id !== id
-            }) 
+            })
             return {
                 ...state,
                 cart: remainingCart,
@@ -28,20 +28,22 @@ const CartReducer = (state , action) => {
         case "clear_cart":
             return {
                 ...state,
-                cart : [],
+                cart: [],
             }
         case "total_bill":
             let initialBill = 0;
-            let totalBill = state.cart.map((curElem) => {
-                return initialBill = initialBill + (curElem.price * curElem.quantity)
-            })
-            console.log(totalBill);
+            if (state.cart != null) {
+                let totalBill = state.cart.map((curElem) => {
+                    return initialBill = initialBill + (curElem.price * curElem.quantity)
+                })
+                console.log(totalBill);
+            }
             return {
                 ...state,
-                total_amount:initialBill,
+                total_amount: initialBill,
 
             }
-    
+
         default:
             return state
     }
